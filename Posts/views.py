c27557c68903933f46070f90593dcdabac3e3878
ShodiyorbekQambaraliyev <shodiyorbek057@gmail.com>
2025-05-04 15:17:12 +0500

    post qoshilish bitdi

--- Posts/views.py
+++ Posts/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Post
 
 def post(request):
-    return render(request, 'post.html')
+    if request.method == "POST":
+        title = request.POST.get("title")
+        image = request.FILES.get("image")
+        if title:
+            Post.objects.create(title=title, image=image)
+        return redirect('post') 
+
+    posts = Post.objects.all()
+    return render(request, 'post.html', {'posts': posts})
